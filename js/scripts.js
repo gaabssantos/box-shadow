d5cce1c447e7a10639f5701d1f6223c527ad75ab
Gabriel Santos <gabriel.oliveira20500@gmail.com>
2023-10-20 16:05:21 -0300

    fetch: finished the copy box rules

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,8 @@ const opacity = document.querySelector("#opacity");
 const shadow = document.querySelector("#shadow");
 
 const box = document.querySelector("#box");
+const ruleValues = document.querySelectorAll(".rule-values");
+const rule = document.querySelector("#rule");
 
 // Functions
 
@@ -23,6 +25,65 @@ const box = document.querySelector("#box");
 [horizontal, vertical, blur, spread, color, opacity].forEach((element) => {
   element.addEventListener("input", (e) => {
     document.querySelector(`#${e.target.id}-text`).value = e.target.value;
-    box.style.boxShadow = `1px 1px 1px #000`;
+    const horizontalValue = horizontal.value;
+    const verticalValue = vertical.value;
+    const blurValue = blur.value;
+    const spreadValue = spread.value;
+    const colorValue = color.value;
+    const opacityValue = opacity.value;
+
+    const hex = colorValue.replace("#", "");
+
+    const r = parseInt(hex.substring(0, 2));
+    const g = parseInt(hex.substring(2, 4));
+    const b = parseInt(hex.substring(4, 6));
+
+    const rgba = "rgba(" + r + "," + g + "," + b + "," + opacityValue + ")";
+
+    const ruleValue = `
+    ${
+      shadow.checked ? "inset" : ""
+    } ${horizontalValue}px ${verticalValue}px ${blurValue}px ${spreadValue}px ${rgba}`;
+
+    box.style.boxShadow = ruleValue;
+
+    ruleValues.forEach((rule) => {
+      rule.innerHTML = ruleValue;
+    });
   });
+});
+
+shadow.addEventListener("input", () => {
+  const horizontalValue = horizontal.value;
+  const verticalValue = vertical.value;
+  const blurValue = blur.value;
+  const spreadValue = spread.value;
+  const colorValue = color.value;
+
+  const hex = colorValue.replace("#", "");
+
+  const r = parseInt(hex.substring(0, 2));
+  const g = parseInt(hex.substring(2, 4));
+  const b = parseInt(hex.substring(4, 6));
+
+  const rgba = "rgba(" + r + "," + g + "," + b + "," + opacityValue + ")";
+
+  const ruleValue = `
+  ${
+    shadow.checked ? "inset" : ""
+  } ${horizontalValue}px ${verticalValue}px ${blurValue}px ${spreadValue}px ${rgba}`;
+
+  box.style.boxShadow = ruleValue;
+
+  ruleValues.forEach((rule) => {
+    rule.innerHTML = ruleValue;
+  });
+});
+
+rule.addEventListener("click", () => {
+  const textCopy = rule.textContent;
+
+  navigator.clipboard.writeText(textCopy);
+
+  alert("Copiado com sucesso.");
 });
